4fa58e1321c22b16fd7cfd1a2458f002d0ac614a
henrymgalvan <hgalvan1971@yahoo.com>
2017-04-09 21:43:38 +0800

    Entity Link initial creation

---
+++ WebLilnksApp/Entity/Link.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebLilnksApp.Entity
+{
+    public class Link
+    {
+        public int Id { get; set; }
+        public string WebLinkName { get; set; }
+        public Uri HttpLink { get; set; }
+        public string Description { get; set; }
+        public string Comment { get; set; }
+        public string Category { get; set; }
+        public string Tags { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public DateTime PublishedDate { get; set; }
+
+    }
+}
